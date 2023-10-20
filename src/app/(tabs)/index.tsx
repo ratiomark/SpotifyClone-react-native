e4d86951eff9adc3376fb7af7287ee333eb734cd
ratiomark <yanagae@gmail.com>
2023-10-21 01:27:06 +0300

    add: recommendation query request GQL

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,9 +1,41 @@
-import { FlatList, Image, StyleSheet } from 'react-native';
+import { ActivityIndicator, ActivityIndicatorBase, FlatList, Image, StyleSheet } from 'react-native';
 import { Text, View } from '../../components/Themed';
-import { tracks } from '../../assets/data/tracks';
 import TrackListItem from '../../components/TrackListItem';
+import { gql, useQuery } from '@apollo/client';
+
+const query = gql`
+	query MyQuery($genres: String) {
+		recommendations(seed_genres: $genres) {
+			tracks {
+				id
+				name
+				preview_url
+				artists {
+					name
+					id
+				}
+				album {
+					id
+					name
+					images {
+						height
+						url
+						width
+					}
+				}
+			}
+		}
+	}
+`
+
 
 export default function HomeScreen() {
+	const { data, loading, error } = useQuery(query, { variables: { genres: 'rock' } })
+
+	if (loading) return <ActivityIndicator />
+
+	const tracks = data?.recommendations?.tracks ?? []
+
 	return (
 		<View style={styles.container}>
 			<FlatList data={tracks}
