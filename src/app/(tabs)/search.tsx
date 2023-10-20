c0166dd97740a294e9b2235999c0ada6ed4e3327
ratiomark <yanagae@gmail.com>
2023-10-21 02:12:05 +0300

    add: Search query request GQL

--- src/app/(tabs)/search.tsx
+++ src/app/(tabs)/search.tsx
@@ -1,12 +1,41 @@
-import { FlatList, TextInput, Image, StyleSheet, Text, View, Keyboard } from 'react-native';
+import { FlatList, TextInput, Image, StyleSheet, Text, View, Keyboard, ActivityIndicator, TouchableOpacity } from 'react-native';
 import { SafeAreaView, } from 'react-native-safe-area-context';
 import { tracks } from '../../assets/data/tracks';
 import TrackListItem from '../../components/TrackListItem';
 import { FontAwesome } from '@expo/vector-icons';
 import React, { useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+const query = gql`
+	query MyQuery($q: String) {
+		search(q: $q) {
+			tracks {
+				items {
+					id
+					preview_url
+					name
+					album {
+						name
+						images {
+							url
+						}
+					}
+					artists {
+						name
+						id
+					}
+				}
+			}
+		}
+	}
+`
 
 export default function SearchScreen() {
 	const [search, setSearch] = useState('')
+	const { data, loading, error } = useQuery(query, { variables: { q: search } })
+
+	const tracks = data?.search?.tracks?.items ?? []
+
 	return (
 
 		<SafeAreaView >
@@ -14,7 +43,6 @@ export default function SearchScreen() {
 
 			{/* <View style={styles.container}> */}
 			<View style={styles.header} >
-
 				<FontAwesome name='search' size={16} color={'gray'} />
 				<TextInput
 					value={search}
@@ -32,7 +60,21 @@ export default function SearchScreen() {
 					Cancel
 				</Text>
 			</View>
-
+			{<FlatList
+				data={["All", "Track", "album", "artist", "playlist", "show", "episode", "audiobook"]}
+				renderItem={({ item }) => (
+					<TouchableOpacity style={{
+						paddingHorizontal: 12, paddingVertical: 5,
+						borderWidth: 1, borderColor: 'white', borderRadius: 999,
+						marginHorizontal: 5,
+					}}>
+						<Text style={{ color: 'white' }}>{item}</Text>
+					</TouchableOpacity>
+				)}
+				style={{ padding: 10 }}
+				horizontal
+			/>}
+			{search !== '' && loading && <ActivityIndicator />}
 			<FlatList
 				data={tracks}
 				renderItem={({ item }) => (
